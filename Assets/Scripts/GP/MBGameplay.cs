3bdaf189b02285b7f5672cc445a9ff05bfeedcbe
ZIKIKIDO\ziki <ziki@kidogames.net>
2026-06-30 15:09:05 +0300

    added mbgameplay

--- Assets/Scripts/GP/MBGameplay.cs
+++ Assets/Scripts/GP/MBGameplay.cs
@@ -1,16 +1,30 @@
+using Common;
+using Core;
+using System.Collections;
 using UnityEngine;
 
-public class MBGameplay : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+namespace qp {
+    public class MBGameplay : MonoBehaviour {
+        private void Awake() {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
+
+        IEnumerator Start() {
+            // load the level 
+
+            // load the board into $Board
+            var board = transform.RecursiveFindChild("$Board");
+
+            // update the borad size accroding to board
+
+            // after update the size of board we will make the scaler to scale it and fit the screen
+            transform.RecursiveFindChild<RTResizeInvoker.ABSMBListener>("$BoardScaler").ForceParentToReport();
+
+            // wait for UI to refresh our Layou+
+            yield return new WaitForEndOfFrame();
+            yield return new WaitForEndOfFrame();
+        }
+
     }
 }
+
